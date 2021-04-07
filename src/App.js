da12c6644f1255916c56804412af266b15bc16d8
HaiNguyenBK0205 <minhhai123yn@gmail.com>
2021-04-07 20:48:11 +0700

    commit nef

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Carousel from "./components/Carousel";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Header/>
+        <Carousel/>
+        <ProductList/>
+        <Footer/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
